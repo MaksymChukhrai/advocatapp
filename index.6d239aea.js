d31db3f5f7f2a35744a176405e3fbd68e33d6a40
MaksymChukhrai <106784292+MaksymChukhrai@users.noreply.github.com>
2024-04-23 23:03:58 +0000

    Deploying to gh-pages from @ MaksymChukhrai/advocatapp@8566f8500beb7235f8294c38acc5408ea4ce67a9 🚀

---
+++ index.6d239aea.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelectorAll("[data-menu-open]"),n=document.querySelector("[data-menu-close]"),t=document.querySelector("[data-menu]");function c(){t.classList.toggle("is-open")}e.forEach(function(e){e.addEventListener("click",c)}),n.addEventListener("click",c)}();
+//# sourceMappingURL=index.6d239aea.js.map
