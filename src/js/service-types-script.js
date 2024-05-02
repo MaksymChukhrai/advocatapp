6d62f67283230157ccb66155a03e5bacc3e008e8
MaksymChukhrai <106784292+MaksymChukhrai@users.noreply.github.com>
2024-05-02 12:51:47 +0300

    _+_

--- src/js/service-types-script.js
+++ src/js/service-types-script.js
@@ -1,5 +1,18 @@
 //src\js\service-types-script.js
 
+
+function redirectToPage(selectElement) {
+  const selectedOption = selectElement.options[selectElement.selectedIndex];
+  const url = selectedOption.getAttribute('data-url');
+
+  if (url) {
+    const isLocalhost = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1';
+    const prefix = isLocalhost ? '' : '/advocatapp';
+    const fullUrl = `${window.location.origin}${prefix}/${url}`;
+    window.location.href = fullUrl;
+  }
+}
+
 document.addEventListener('DOMContentLoaded', () => {
     const currentPageUrl = window.location.href;
     const selectElement = document.getElementById('service');
