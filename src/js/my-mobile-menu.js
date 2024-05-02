54a5e6bc5b89508e629db3b3de17b46adef8573e
MaksymChukhrai <106784292+MaksymChukhrai@users.noreply.github.com>
2024-05-03 00:41:36 +0300

    _+_

--- src/js/my-mobile-menu.js
+++ src/js/my-mobile-menu.js
@@ -1,19 +1,18 @@
 // src\js\my-mobile-menu.js
 
 (() => {
-    const openMenuBtns = document.querySelectorAll("[data-menu-open]");
-    const closeMenuBtn = document.querySelector("[data-menu-close]");
-    const menu = document.querySelector("[data-menu]");
-  
-    openMenuBtns.forEach(btn => {
-      btn.addEventListener("click", toggleMenu);
-      console.log('Клик по кнопке открытия моб меню');
-    });
-  
-    closeMenuBtn.addEventListener("click", toggleMenu);
-  
-    function toggleMenu() {
-      menu.classList.toggle("is-open");
-    }
-  })();
-  
+  const openMenuBtns = document.querySelectorAll("[data-menu-open]");
+  const closeMenuBtn = document.querySelector("[data-menu-close]");
+  const menu = document.querySelector("[data-menu]");
+
+  openMenuBtns.forEach(btn => {
+    btn.addEventListener("click", toggleMenu);
+    console.log('Клик по кнопке открытия моб меню');
+  });
+
+  closeMenuBtn.addEventListener("click", toggleMenu);
+
+  function toggleMenu() {
+    menu.classList.toggle("is-open");
+  }
+})();
