6400f07f1aca812c0cb17f8767120722db549fc0
MaksymChukhrai <106784292+MaksymChukhrai@users.noreply.github.com>
2024-05-02 16:50:01 +0300

    _+_

--- src/js/service-types-script.js
+++ src/js/service-types-script.js
@@ -1,32 +1,27 @@
 //src\js\service-types-script.js
 
-
 function redirectToPage(selectElement) {
   const selectedOption = selectElement.options[selectElement.selectedIndex];
   const url = selectedOption.getAttribute('data-url');
 
   if (url) {
-    const isLocalhost = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1';
-    const prefix = isLocalhost ? '' : '/advocatapp';
-    const fullUrl = `${window.location.origin}${prefix}/${url}`;
-    window.location.href = fullUrl;
+    window.location.href = url;
   }
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-    const currentPageUrl = window.location.href;
-    const selectElement = document.getElementById('service');
-  
-    // Отметить выбранный элемент в выпадающем списке
-    if (selectElement) {
-      const options = selectElement.options;
-      for (let i = 0; i < options.length; i++) {
-        const optionUrl = options[i].getAttribute('data-url');
-        if (optionUrl && currentPageUrl.includes(optionUrl)) {
-          options[i].selected = true;
-          break;
-        }
+  const currentPageUrl = window.location.pathname.split('/').pop(); // Получаем имя текущего файла
+  const selectElement = document.getElementById('service');
+
+  if (selectElement) {
+    const options = selectElement.options;
+    for (let i = 0; i < options.length; i++) {
+      const optionUrl = options[i].getAttribute('data-url');
+      if (optionUrl === currentPageUrl) {
+        options[i].selected = true;
+        break;
       }
     }
-  });
+  }
+});
   
