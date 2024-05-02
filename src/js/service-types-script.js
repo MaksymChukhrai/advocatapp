9ed4bbae6067af6cebe744f0d7b46e11673bb5bc
MaksymChukhrai <106784292+MaksymChukhrai@users.noreply.github.com>
2024-05-02 18:24:18 +0300

    _+_

--- src/js/service-types-script.js
+++ src/js/service-types-script.js
@@ -16,12 +16,21 @@ document.addEventListener('DOMContentLoaded', () => {
   if (selectElement) {
     const options = selectElement.options;
     for (let i = 0; i < options.length; i++) {
-      const optionUrl = 'src/' + options[i].getAttribute('data-url');
+      const optionUrl = options[i].getAttribute('data-url');
       if (optionUrl === currentPageUrl) {
         options[i].selected = true;
         break;
       }
     }
+
+    // Добавляем обработчик события change
+    selectElement.addEventListener('change', () => {
+      const selectedOption = selectElement.options[selectElement.selectedIndex];
+      const url = selectedOption.getAttribute('data-url');
+      if (url) {
+        window.location.href = url;
+      }
+    });
   }
 });
   
