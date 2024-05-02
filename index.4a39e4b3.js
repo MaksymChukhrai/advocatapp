63f85f3f8b20396aa81e1e8ff460f14cff7d5fda
MaksymChukhrai <106784292+MaksymChukhrai@users.noreply.github.com>
2024-05-02 21:42:09 +0000

    Deploying to gh-pages from @ MaksymChukhrai/advocatapp@54a5e6bc5b89508e629db3b3de17b46adef8573e 🚀

---
+++ index.4a39e4b3.js
@@ -0,0 +1,2 @@
+(()=>{let e=document.querySelectorAll("[data-menu-open]"),t=document.querySelector("[data-menu-close]"),c=document.querySelector("[data-menu]");function n(){c.classList.toggle("is-open")}e.forEach(e=>{e.addEventListener("click",n),console.log("Клик по кнопке открытия моб меню")}),t.addEventListener("click",n)})();
+//# sourceMappingURL=index.4a39e4b3.js.map
