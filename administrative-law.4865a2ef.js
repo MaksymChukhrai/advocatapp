f1def36afb37da726e49867e0e8610d609ee8ee9
MaksymChukhrai <106784292+MaksymChukhrai@users.noreply.github.com>
2024-05-02 13:50:30 +0000

    Deploying to gh-pages from @ MaksymChukhrai/advocatapp@6400f07f1aca812c0cb17f8767120722db549fc0 🚀

---
+++ administrative-law.4865a2ef.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",function(){var e=window.location.pathname.split("/").pop(),t=document.getElementById("service");if(t){for(var n=t.options,o=0;o<n.length;o++)if(n[o].getAttribute("data-url")===e){n[o].selected=!0;break}}});
+//# sourceMappingURL=administrative-law.4865a2ef.js.map
--- administrative-law.71cfafea.js
+++
@@ -1,2 +0,0 @@
-document.addEventListener("DOMContentLoaded",function(){var e=window.location.href,t=document.getElementById("service");if(t)for(var n=t.options,d=0;d<n.length;d++){var o=n[d].getAttribute("data-url");if(o&&e.includes(o)){n[d].selected=!0;break}}});
-//# sourceMappingURL=administrative-law.71cfafea.js.map
