9518ac0f112529286fbb09d82bff3795ebcdbf5f
MaksymChukhrai <106784292+MaksymChukhrai@users.noreply.github.com>
2024-05-08 16:55:39 +0000

    Deploying to gh-pages from @ MaksymChukhrai/advocatapp@ef3dc3da1ede6bca564d174f38c4a7fb0d778211 🚀

---
+++ cases.47a5ad51.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",function(){let e=document.querySelectorAll(".steps-container");function t(){e.forEach((e,t)=>{let n=e.getBoundingClientRect();n.top>=0&&n.bottom<=window.innerHeight&&(e.style.opacity=1,e.style.transform="translateX(0)")})}window.addEventListener("scroll",t),window.addEventListener("resize",t),t()});
+//# sourceMappingURL=cases.47a5ad51.js.map
