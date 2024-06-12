0cd9382f4ff31e4a723fb1ac283a6f9726f1c97e
MaksymChukhrai <106784292+MaksymChukhrai@users.noreply.github.com>
2024-06-13 01:58:03 +0300

    _+_

--- src/js/service-types-sidebar-script.js
+++ src/js/service-types-sidebar-script.js
@@ -1,68 +1,14 @@
 // src/js/service-types-sidebar-script.js
+import $ from 'jquery';
 
-document.addEventListener('DOMContentLoaded', function() {
+$(window).on('load', () => {
+  const currentUrl = window.location.pathname.split('/').pop();
 
-    const links = document.querySelectorAll('.sidebar a');
-    const content = document.getElementById('content');
-    console.log('Content element:', content);
+  const $links = $('.service-buttons li a');
+  const $currentLink = $(`.service-buttons li a[href="/${currentUrl}"]`);
 
-    links.forEach(link => {
-        link.addEventListener('click', function(event) {
-            event.preventDefault();
-            const url = this.getAttribute('data-url');
-            
-            // Подгрузка контента услуги
-            fetch(url)
-                .then(response => response.text())
-                .then(data => {
-                    // Извлечение контента между тегами <body>
-                    const parser = new DOMParser();
-                    const doc = parser.parseFromString(data, 'text/html');
-                    const newContent = doc.querySelector('#content').innerHTML;
-
-                    content.innerHTML = newContent;
-
-                    // Обновление истории браузера
-                    history.pushState(null, '', url);
-
-                    // Обновление активного класса
-                    links.forEach(link => link.parentElement.classList.remove('select'));
-                    this.parentElement.classList.add('select');
-                    console.log('Added select class to', this.parentElement); // Проверка добавления класса
-                })
-                .catch(error => console.error('Ошибка загрузки:', error));
-        });
-    });
-
-    // Обработка изменения состояния истории (при навигации назад/вперед)
-    window.addEventListener('popstate', function() {
-        const url = location.pathname.split('/').pop();
-        fetch(url)
-            .then(response => response.text())
-            .then(data => {
-                const parser = new DOMParser();
-                const doc = parser.parseFromString(data, 'text/html');
-                const newContent = doc.querySelector('#content').innerHTML;
-
-                content.innerHTML = newContent;
-
-                // Обновление активного класса
-                links.forEach(link => link.parentElement.classList.remove('select'));
-                const activeLink = document.querySelector(`a[data-url="${url}"]`);
-                console.log('Active link:', activeLink);
-                if (activeLink) {
-                    activeLink.parentElement.classList.add('select');
-                    console.log('Added select class to', activeLink.parentElement); // Проверка добавления класса
-                }
-            })
-            .catch(error => console.error('Ошибка загрузки:', error));
-    });
-
-    // Проверка начального состояния для текущей страницы
-    const initialUrl = location.pathname.split('/').pop();
-    const initialActiveLink = document.querySelector(`a[data-url="${initialUrl}"]`);
-    if (initialActiveLink) {
-        initialActiveLink.parentElement.classList.add('select');
-        console.log('Added select class to', initialActiveLink.parentElement); // Проверка добавления класса
-    }
+  if ($currentLink.length > 0) {
+    Array.from($links).forEach(link => $(link).parent().removeClass('select'));
+    Array.from($currentLink).forEach(link => $(link).parent().addClass('select'));
+  }
 });
