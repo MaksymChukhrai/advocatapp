72a7150961bce14fa6e330a368b5934b6ca67dd0
MaksymChukhrai <106784292+MaksymChukhrai@users.noreply.github.com>
2024-06-18 00:38:23 +0000

    Deploying to gh-pages from @ MaksymChukhrai/advocatapp@43f22642266d348640b5a5b72977fe430ea3187e 🚀

---
+++ aboutme.57d46585.js
@@ -0,0 +1,2 @@
+(()=>{let e=document.querySelectorAll("[data-menu-open]"),t=document.querySelector("[data-menu-close]"),c=document.querySelector("[data-menu]");function n(){c.classList.toggle("is-open")}e.forEach(e=>{e.addEventListener("click",n)}),t.addEventListener("click",n)})();
+//# sourceMappingURL=aboutme.57d46585.js.map
